3b40009cfeb1c41e26fa9b171e7cf7668dec95a0
Monisa Felson <80293018+felsonm@users.noreply.github.com>
2023-08-12 00:36:15 -0400

    Update registration.js

--- registration.js
+++ registration.js
@@ -14,18 +14,44 @@ if (testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com')
     console.log('TASK 1: PASS');
 }
 
-class Student {
-    constructor(name, email) {
-        this.name = name;
-        this.email = email;
-    }
-}
-
 class Bootcamp {
     constructor(name, level, students = []) {
         this.name = name;
         this.level = level;
         this.students = students;
+
+    }
+
+    registerStudent(studentToRegister) {
+        if (!studentToRegister.name || !studentToRegister.email) {
+            console.log('Invalid name or email');
+            return false;
+        }
+
+        for (const student of this.students) {
+            if (student.email === studentToRegister.email) {
+                console.log('Email is already registered');
+                return false;
+            }
+
+        }
+        this.students.push(studentToRegister);
+        console.log(`Successfully registered ${studentToRegister.name} for ${this.name}`);
+        return true;
+
+    }
+
+    listStudents() {
+        if (this.students.length === 0) {
+            console.log(`No students are registered to the ${this.name} bootcamp.`);
+            return false;
+        } else {
+            console.log(`The students registered in ${this.name} are:`);
+            for (const student of this.students) {
+                console.log(`Name: ${student.name}, Email: ${student.email}`);
+            }
+            return true;
+        }
     }
 }
 
@@ -37,3 +63,24 @@ if (reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced'
     console.log('TASK 2: PASS');
 }
 
+
+// Test function definition
+
+
+const runTest = (bootcamp, student) => {
+    const attemptOne = bootcamp.registerStudent(student);
+    const attemptTwo = bootcamp.registerStudent(student);
+    const attemptThree = bootcamp.registerStudent(new Student("Babs Bunny"));
+    if (attemptOne && !attemptTwo && !attemptThree) {
+        console.log("TASK 3: PASS");
+    }
+
+    bootcamp.registerStudent(new Student('Babs Bunny', 'babs@bunny.com'));
+    if (bootcamp.listStudents()) {
+        console.log("TASK 4: PASS 1/2");
+    }
+    bootcamp.students = [];
+    if (!bootcamp.listStudents()) {
+        console.log("TASK 4: PASS 2/2");
+    }
+};
